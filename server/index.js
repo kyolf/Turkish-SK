f5384ea75e374948bcf78705bc007976ccc156d1
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-07 13:04:33 -0700

    got all authentication working

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
+'use strict';
 const path = require('path');
 const express = require('express');
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const BearerStrategy = require('passport-http-bearer').Strategy;
+
+const {User} = require('./models/models_user.js');
 
 let secret = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET
-}
+};
 
 if(process.env.NODE_ENV != 'production') {
   secret = require('./secret');
@@ -21,104 +25,172 @@ const database = {
 app.use(passport.initialize());
 
 passport.use(
-    new GoogleStrategy({
-        clientID:  secret.CLIENT_ID,
-        clientSecret: secret.CLIENT_SECRET,
-        callbackURL: `/api/auth/google/callback`
-    },
-    (accessToken, refreshToken, profile, cb) => {
-        // Job 1: Set up Mongo/Mongoose, create a User model which store the
-        // google id, and the access token
-        // Job 2: Update this callback to either update or create the user
-        // so it contains the correct access token
-        const user = database[accessToken] = {
-            googleId: profile.id,
-            accessToken: accessToken
-        };
-        return cb(null, user);
-    }
+  new GoogleStrategy({
+    clientID:  secret.CLIENT_ID,
+    clientSecret: secret.CLIENT_SECRET,
+    callbackURL: '/api/auth/google/callback'
+  },
+  (accessToken, refreshToken, profile, cb) => {
+      // Job 1: Set up Mongo/Mongoose, create a User model which store the
+      // google id, and the access token
+    console.log(profile.id, accessToken);
+    let message;
+    const newUser = {
+      googleId: profile.id,
+      accessToken,
+      numCorrect: 0,
+      numQuestAns: 0,
+      questId: 0
+    };
+
+    // User
+    // .findOneAndUpdate({googleId:profile.id}, {$set:{accessToken}}, {new:true})
+    User
+    .findOne({googleId:profile.id}, (err, user) => {
+      if(err){
+        message = `Finding a user in database error: ${err}`;
+        return cb(err, {message});
+      }
+      return user;
+    })
+    .then(result=>{
+      if(result){
+        User.findOneAndUpdate({googleId:profile.id}, {$set:{accessToken}}, {new:true}, (err, doc) => {
+          if(err){
+            message = `Updating a user in database error: ${err}`;
+            return cb(err, {message});
+          }
+          return cb(err, doc);
+        });
+      }
+      else{
+        User.create(newUser, (err, user) => {
+          if(err){
+            message = `Creating a user in database error: ${err}`;
+            return cb(err, {message});
+          }
+          return cb(err, user);
+        });
+      }
+    });
+  
+    // Job 2: Update this callback to either update or create the user
+    // so it contains the correct access token
+    // const user = database[accessToken] = {
+    //   googleId: profile.id,
+    //   accessToken: accessToken
+    // };
+    // return cb(null, user);
+  }
 ));
 
 passport.use(
-    new BearerStrategy(
-        (token, done) => {
-            // Job 3: Update this callback to try to find a user with a
-            // matching access token.  If they exist, let em in, if not,
-            // don't.
-            if (!(token in database)) {
-                return done(null, false);
-            }
-            return done(null, database[token]);
+  new BearerStrategy(
+    (token, done) => {
+      // Job 3: Update this callback to try to find a user with a
+      // matching access token.  If they exist, let em in, if not,
+      // don't.
+      console.log('token:', token);
+      User
+      .findOne({accessToken:token})
+      .exec()
+      .then(user =>{
+        console.log(user);
+        if(!user){
+          return done(null, false);
         }
-    )
+        return done(null, user);
+      });
+      // if (!(token in database)) {
+      //   return done(null, false);
+      // }
+      // return done(null, database[token]);
+    }
+  )
 );
 
 app.get('/api/auth/google',
-    passport.authenticate('google', {scope: ['profile']}));
+  passport.authenticate('google', {scope: ['profile']}));
 
 app.get('/api/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/',
-        session: false
-    }),
-    (req, res) => {
-        res.cookie('accessToken', req.user.accessToken, {expires: 0});
-        res.redirect('/');
-    }
+  passport.authenticate('google', {
+    failureRedirect: '/',
+    session: false
+  }),
+  (req, res) => {
+    res.cookie('accessToken', req.user.accessToken, {expires: 0});
+    res.redirect('/');
+  }
 );
 
 app.get('/api/auth/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('accessToken');
-    res.redirect('/');
+  req.logout();
+  res.clearCookie('accessToken');
+  res.redirect('/');
 });
 
 app.get('/api/me',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json({
-        googleId: req.user.googleId
-    })
+  passport.authenticate('bearer', {session: false}),
+  (req, res) => res.json({
+    googleId: req.user.googleId
+  })
 );
 
 app.get('/api/questions',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json(['Question 1', 'Question 2'])
+  passport.authenticate('bearer', {session: false}),
+  (req, res) => res.json(['Question 1', 'Question 2'])
 );
 
+app.get('/api/users', (req,res) => {
+  User
+  .find()
+  .exec()
+  .then(users => res.json(users.map(user => user.apiRepr())));
+});
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Unhandled requests which aren't for the API should serve index.html so
 // client-side routing using browserHistory can function
 app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
+  const index = path.resolve(__dirname, '../client/build', 'index.html');
+  res.sendFile(index);
 });
 
 let server;
-function runServer(port=3001) {
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            resolve();
-        }).on('error', reject);
+function runServer(port=3001, databaseUrl = secret.DATABASE_URL) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if(err) {
+        reject(err);
+      }
+      server = app.listen(port, () => {
+        resolve();
+      }).on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
+  });
 }
 
 function closeServer() {
+  return mongoose.disconnect().then(() => {
     return new Promise((resolve, reject) => {
-        server.close(err => {
-            if (err) {
-                return reject(err);
-            }
-            resolve();
-        });
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
+  });
 }
 
 if (require.main === module) {
-    runServer();
+  runServer();
 }
 
 module.exports = {
-    app, runServer, closeServer
+  app, runServer, closeServer
 };
