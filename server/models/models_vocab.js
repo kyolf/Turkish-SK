13e57c5c27599ea34660bff42a08984b6b61e116
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-08 10:53:09 -0700

    made a apiRepr for vocabSchema

--- server/models/models_vocab.js
+++ server/models/models_vocab.js
@@ -8,6 +8,15 @@ const vocabSchema = mongoose.Schema({
   questId:{type:Number, required:true}
 });
 
+vocabSchema.methods.apiRepr = function(){
+  return {
+    id: this._id,
+    turkWord: this.turkWord,
+    engWord: this.engWord,
+    questId: this.questId
+  };
+};
+
 const Vocab = mongoose.model('Vocab', vocabSchema);
 
 module.exports = {Vocab};
