4cddaaa26288a1fc2d9a8a9fff518549507539a5
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-09 09:28:15 -0700

    implemented the conversion from arr to linked list and vice versa

--- client/src/linkedList.js
+++ client/src/linkedList.js
@@ -5,8 +5,8 @@ class LinkedList{
     this.head = null;
   }
 
-  insert(curNode, value){
-    let nodeIns ={value};
+  insert(curNode, turkWord, engWord, questId, weight){
+    let nodeIns ={turkWord, engWord, questId, weight};
 
     if(!curNode){
       nodeIns.next = this.head;
@@ -14,7 +14,32 @@ class LinkedList{
     }
     else{
       nodeIns.next = curNode.next;
-      curNode.next= nodeIns;
+      curNode.next = nodeIns;
+    }
+  }
+
+  insertAll(arr){
+    let node;
+    for(let i = 0; i < arr.length; i++){
+      const nodeIns = {
+        turkWord: arr[i].turkWord,
+        engWord: arr[i].engWord,
+        questId: arr[i].questId,
+        weight: arr[i].weight
+      };
+
+      const curNode = node;
+
+      if(!this.head){
+        nodeIns.next = this.head;
+        this.head = nodeIns;
+        node = this.head;
+      }
+      else{
+        nodeIns.next = curNode.next;
+        curNode.next = nodeIns;
+        node = node.next;
+      }
     }
   }
 
@@ -22,10 +47,30 @@ class LinkedList{
     if(!this.head){
       return null;
     }
+    
     let nextHead = this.head.next;
     this.head.next = null;
     let deletedNode = this.head;
     this.head = nextHead;
     return deletedNode;
+  }
+
+  deleteAll(){
+    let arr = [];
+    
+    if(!this.head){
+      return arr;
+    }
+
+    while(this.head !== null){
+      const deleted = this.delete();
+      arr.push({
+        turkWord: deleted.turkWord,
+        engWord: deleted.engWord,
+        questId: deleted.questId,
+        weight: deleted.weight
+      });
+    }
+    return arr;
   }
 }
