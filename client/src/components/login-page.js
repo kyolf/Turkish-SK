270537cf1bb1c67e6f3040776d89c9091489b758
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-08-11 03:51:04 -0700

    code consitency with login page

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './login-page.css';
 
 export default function LoginPage() {
-    return (
-        <div className='LoginPage'>
-          <a href={'/api/auth/google'}><button>
-            Login with Google
-          </button>
-          </a>
-        </div>
-    );
+  return (
+    <div className='LoginPage'>
+      <a href={'/api/auth/google'}>
+        <button>
+          Login with Google
+        </button>
+      </a>
+    </div>
+  );
 }
