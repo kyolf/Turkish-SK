0b809107c9ba1ce36ad43aaf54ad03ff20c53e51
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-09-16 21:06:37 -0700

    made everything responsive with the image

--- client/src/__tests__/info.js
+++ client/src/__tests__/info.js
@@ -7,7 +7,7 @@ describe('Login Test', () => {
     shallow(<InfoPage />);
   });
 
-  it('has a div, a main, 3 section, 3 p, 3 h2, and a h1 element', () => {
+  it('has a div, a main, 3 section, 3 p, 3 h2, 3 img, and a h1 element', () => {
     const wrapper = shallow(<InfoPage />);
     expect(wrapper.find('div').length).toEqual(1);
     expect(wrapper.find('main').length).toEqual(1);
@@ -15,5 +15,6 @@ describe('Login Test', () => {
     expect(wrapper.find('section').length).toEqual(3);
     expect(wrapper.find('h2').length).toEqual(3);
     expect(wrapper.find('p').length).toEqual(3);
+    expect(wrapper.find('img').length).toEqual(3);
   });
 });
--- client/src/components/info-page.js
+++ client/src/components/info-page.js
@@ -8,17 +8,20 @@ export default function InfoPage() {
       <h1>Why you use our app?</h1>
       <main>
         <section className='info_block'>
+          <img src='./frontend_images/clueless.png' alt='beginner'/>
           <h2>Beginner Friendly</h2>
           <p>Don't know any Turkish? Don't worry about it! When you get Turkish words wrong, a flash card 
              will show up and show the English meaning that corresponds to the Turkish word!</p>
         </section>
         <section className='info_block middle'>
+          <img src='./frontend_images/learning.png' alt='learning'/>
           <h2>Spaced Repetition</h2>
           <p>Don't be afraid to get answers wrong! Wrong answers will show up more 
            often to ensure that you remember the English meaning of a Turkish word!</p>
         </section>
         <section className='info_block'>
-          <h2>Start at where you left off</h2>
+          <img src='./frontend_images/save_file.png' alt='save'/>
+          <h2>Saves Progress</h2>
           <p>Don't worry about accidentally closing or refreshing the page! The app will
              save your progress everytime you submit an answer!</p>
         </section>
